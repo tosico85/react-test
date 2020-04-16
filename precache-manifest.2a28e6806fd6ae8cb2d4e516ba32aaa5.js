9383de7f1504d8f114d04eeb62f118712a671547
kim deokhyeon <tosico85@gmail.com>
2020-04-16 18:31:25 +0900

    Updates

---
+++ precache-manifest.2a28e6806fd6ae8cb2d4e516ba32aaa5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddd5429ce85fd6540a25adf1fd827bc",
+    "url": "/react-test/index.html"
+  },
+  {
+    "revision": "6b8dc2f07329caa809cb",
+    "url": "/react-test/static/css/main.6c3e52b0.chunk.css"
+  },
+  {
+    "revision": "752770e5038488546543",
+    "url": "/react-test/static/js/2.65e6826d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-test/static/js/2.65e6826d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8dc2f07329caa809cb",
+    "url": "/react-test/static/js/main.74578276.chunk.js"
+  },
+  {
+    "revision": "40958326a7a5e8d40d0e",
+    "url": "/react-test/static/js/runtime-main.79a7e98c.js"
+  }
+]);
